e8fef9de85002382b171629e5e83b507f686c89b
Marcos Montiel <marcos-gab14@hotmail.com>
2023-07-25 17:09:41 -0600

    Functions file was modified

--- app/src/main/java/com/marcosmontiel/basicswithkotlin/Functions.kt
+++ app/src/main/java/com/marcosmontiel/basicswithkotlin/Functions.kt
@@ -15,3 +15,8 @@ fun operation(first: Int, second: Int): Int = first + second
 // Extended function
 
 fun Int.obtainAbs(isActive: Boolean = true): Int = if (isActive) abs(this) else this
+
+// Override function
+
+fun addItem(name: String, quantity: Int = 0, discount: Int = 0): String =
+    "Product: $name, Quantity: $quantity, Discount: $discount"
--- app/src/main/java/com/marcosmontiel/basicswithkotlin/Principal.kt
+++ app/src/main/java/com/marcosmontiel/basicswithkotlin/Principal.kt
@@ -1,10 +1,16 @@
 package com.marcosmontiel.basicswithkotlin
 
 fun main() {
+
     newTopic("Hello new world")
     newTopic(operation(50, 50))
 
     val number = -5
     newTopic(number.obtainAbs())
     newTopic(number.obtainAbs(false))
+
+    newTopic(addItem("7-Up", 20, 10))
+    newTopic(addItem("Coca Cola", 100))
+    newTopic(addItem("Pepsi"))
+
 }
